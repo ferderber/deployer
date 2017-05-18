7da515e45497cff490e13b55b8be3b8c1a9b2c34
Matthew Ferderber <matthewferderber@gmail.com>
2017-05-18 01:04:18 -0400

    make directory before sync

--- web_deploy.sh
+++ web_deploy.sh
@@ -6,5 +6,5 @@ git clone $1 \
     && cd /tmp/$2 \
     && yarn \
     && yarn run build \
-    && rsync -a --delete /tmp/$2/dist /var/www/$3 \
-    && mv /var/www/$3/dist /var/www/$3/$2
+    && mkdir -p /var/www/$3/$2 \
+    && rsync -a --delete /tmp/$2/dist/ /var/www/$3/$2 \
