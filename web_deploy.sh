44e75c07202194f8723a68e4cc9858ed81ba517c
Matthew Ferderber <matthewferderber@gmail.com>
2018-01-20 15:55:50 -0500

    force install devDependencies

--- web_deploy.sh
+++ web_deploy.sh
@@ -4,7 +4,7 @@ git clone $1 \
     --depth 1 \
     /tmp/$2 \
     && cd /tmp/$2 \
-    && yarn \
+    && yarn --production=false \
     && yarn run build \
     && mkdir -p /var/www/$3/$2 \
     && rsync -a --delete /tmp/$2/dist/ /var/www/$3/$2
